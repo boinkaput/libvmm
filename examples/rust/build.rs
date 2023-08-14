fd330f6dd194432b1d66780aeabd90adb150420c
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-14 23:19:47 +1000

    [do not merge]: Rust example now in working state

--- examples/rust/build.rs
+++ examples/rust/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-search=/home/ivanv/ts/sel4cp_dev/release/sel4cp-sdk-1.2.6/board/qemu_arm_virt/debug/lib/");
+    println!("cargo:rustc-link-search=/home/ivanv/ts/sel4cp/release/sel4cp-sdk-1.2.6/board/qemu_arm_virt/debug/lib/");
     println!("cargo:rustc-link-search=/home/ivanv/ts/sel4cp_vmm/examples/rust/build");
 }
 
