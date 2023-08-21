74e1f80ddd9f545355db0941179e4290b728010e
tim-arney <t.arney@unsw.edu.au>
2023-08-21 12:58:14 +1000

    [WIP] various fixes to ethernet code

--- tools/linux/ethernet.c
+++ tools/linux/ethernet.c
@@ -1,13 +1,21 @@
-#include <sys/mman.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#include <net/if.h>
-#include <sys/socket.h>
+
+#include <arpa/inet.h>
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
-#include <string.h>
-#include <errno.h>
-#include <stdio.h>
+#include <net/if.h>
+
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+
 #include "shared_ringbuffer.h"
+
 
 /*
  * It'd be nice to use PACKET_TX_RING and PACKET_RX_RING to get
@@ -24,11 +32,11 @@ struct sddf_comms {
     struct ring_buffer *tx_empty;
     struct ring_buffer *rx_inuse;
     struct ring_buffer *rx_empty;
-    unsogned char *ring_physbase;
+    unsigned char *ring_physbase;
     unsigned char *ring_base;
-    unsogned char *tx_buf_physbase;
+    unsigned char *tx_buf_physbase;
     unsigned char *tx_buf_base;
-    unsogned char *rx_buf_physbase;
+    unsigned char *rx_buf_physbase;
     unsigned char *rx_buf_base;
 };
 
@@ -39,14 +47,16 @@ struct buf_desc {
 
 
 static int
-getbuf(char *base, struct ring_buffer *rp, struct buf_desc *bp)
+getbuf(unsigned char *base, struct ring_buffer *rp, struct buf_desc *bp)
 {
     if (dequeue(rp, &bp->payload, &bp->len, NULL))
         return 0;
     bp->payload -= physbase; 
-    bp->payload += sbase;
+    bp->payload += base;
     return 1;
 }
+
+// putbuf(struct buf_desc *bp, unsigned char *base, ...)
 
 /*
  * Set up a raw socket in promiscuous mode
@@ -59,25 +69,25 @@ setup_socket(const char *devicename, int *ifindex)
     int s;
     s = socket(AF_PACKET, SOCK_RAW|SOCK_NONBLOCK, htons(ETH_P_ALL));
 
-    strncpy(ifr.ifname, devicename, sizeof ifr.ifname);
-    if (-1 == ioctl(s, SIOGIFFLAFS, &ifr)) {
+    strncpy(ifr.ifr_name, devicename, sizeof ifr.ifr_name);
+    if (-1 == ioctl(s, SIOCGIFFLAGS, &ifr)) {
         perror("Error: could not get device flags");
         close(s);
         return -1;
     }
 
     ifr.ifr_flags |= IFF_PROMISC;
-    if (-1 == ioctl(s, SIOCSFLAGS, $ifr)) {
+    if (-1 == ioctl(s, SIOCSIFFLAGS, &ifr)) {
         perror("Error: could not set device flags");
         close(s);
         return -1;
     }
     if (ioctl(s, SIOCGIFINDEX, &ifr))
     {
-        perror("Error: Could not get  device index");
+        perror("Error: Could not get device index");
     }
 
-    *ifindex = ifr.ifr_index;
+    *ifindex = ifr.ifr_ifindex;
     return s;
 }
 
@@ -94,7 +104,7 @@ long int_from_file(char *f)
         perror(f);
         return -1;
     }
-    if (scanf(fin, "%ld", &i) != 1) {
+    if (fscanf(fin, "%ld", &i) != 1) {
         fprintf(stderr, "%s: %s\n", f, strerror(errno));
         fclose(fin);
         return -1;
@@ -114,7 +124,7 @@ setup_uio(int devicenum, struct sddf_comms *sdfp) {
     char sysname[64];
     int uiofd;
     int strend; /* index of end of sysfs uio maps pathname */
-    char *mappings[NMAPPINGS];
+    unsigned char *mappings[NMAPPINGS];
     int i;
     uintptr_t addr[NMAPPINGS];
     size_t sz[NMAPPINGS];
@@ -136,7 +146,7 @@ setup_uio(int devicenum, struct sddf_comms *sdfp) {
 
         /* If we can map 1:1 it'll be easier. */
         mappings[i] = mmap(addr, sz, PROT_READ|PROT_WRITE, MAP_SHARED,
-                           uiofd, i * getpageszize());
+                           uiofd, i * getpagesize());
         if (mappings[i] == MAP_FAILED) {
             fprintf(stderr, "mapping of region %d failed: %s\n",
                     i, strerror(errno));
@@ -155,12 +165,12 @@ setup_uio(int devicenum, struct sddf_comms *sdfp) {
      * Mapping 1: Rx buffers
      */
     sdfp->rx_buf_base = mappings[1];
-    sdfp->rx_phys_base = addr[1];
+    sdfp->rx_buf_physbase = addr[1];
     /*
      * Mapping 2: Tx buffers
      */
     sdfp->tx_buf_base = mappings[2];
-    sdfp->tx_phys_base = addr[2];
+    sdfp->tx_buf_physbase = addr[2];
 
     setup_rings(sdfp, addr, sz);
     return uiofd;
@@ -173,7 +183,7 @@ mainloop(int uiofd, int sock, int ifindex, struct sddf_comms *sdfp)
     struct pollfd[2];
 #define UIOFD 0
 #define SOCKFD 1
-    struct sockaddr ll;
+    struct sockaddr_ll ll;
     struct buf_desc txbuf;
     struct buf_desc rxbuf;
     pollfd[UIOFD].fd = uiofd;
